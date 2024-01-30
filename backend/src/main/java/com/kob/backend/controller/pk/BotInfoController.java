c3ba9f6cd62b292d73cae4249f9500067ccc6907
ZeroAC <1245271811@qq.com>
2024-01-30 17:38:04 +0800

    SpringBoot项目前后端新建

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,31 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zeroac
+ */
+@RestController
+@RequestMapping("/pk")
+public class BotInfoController {
+
+    @RequestMapping("/getBotInfo")
+    public List<Map<String, String>> getBotInfo() {
+        List<Map<String, String>> list = new ArrayList<>();
+        Map<String, String> kob1 = new HashMap<>();
+        kob1.put("name", "jack");
+        kob1.put("score", "1100");
+        Map<String, String> kob2 = new HashMap<>();
+        kob2.put("name", "tom");
+        kob2.put("score", "1500");
+        list.add(kob1);
+        list.add(kob2);
+        return list;
+    }
+}
