ec6fe7726fc981a089d3216d100c99c62afd9d60
ZeroAC <1245271811@qq.com>
2024-02-05 09:16:15 +0800

    添加重新生成地图的按钮

--- frontend/web/src/components/PlayGround.vue
+++ frontend/web/src/components/PlayGround.vue
@@ -1,6 +1,13 @@
 <template lang="">
-  <div class="playground">
-    <game-map></game-map>
+  <div class="playground row">
+    <div class="col-sm-10">
+      <game-map :key="componentKey"></game-map>
+    </div>
+    <div class="col-sm-2 d-flex align-items-center justify-content-center">
+      <button class="btn btn-primary" @click="refreshComponent">
+        重新生成
+      </button>
+    </div>
   </div>
 </template>
 <script>
@@ -9,13 +16,28 @@ export default {
   components: {
     GameMap,
   },
+  data() {
+    return {
+      componentKey: 0,
+    };
+  },
+  methods: {
+    refreshComponent() {
+      // 点击按钮时改变 key 的值，触发组件重新渲染
+      this.componentKey++;
+    },
+  },
 };
 </script>
 <style scoped>
 .playground {
   width: 60vw;
   height: 80vh;
-  background-color: rgb(21, 235, 250);
+  /* background: radial-gradient(
+    circle,
+    rgba(8, 207, 243, 0.8),
+    rgba(6, 25, 1, 0.6)
+  ); */
   margin: 5vh auto;
 }
 </style>
